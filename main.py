974d7c4918dcdaaad4ea6403d1cd6fbcffa5efa0
Aleksandrinsider <102919197+Aleksandrinsider@users.noreply.github.com>
2025-12-29 23:14:16 +0300

    Return debug data again

--- main.py
+++ main.py
@@ -41,13 +41,7 @@ async def simple_login_handler(request):
 
 async def auth_handler(request):
     data = request.query
-    if check_telegram_authentication(data):
-        user_id = int(data['id'])
-        session = await get_session(request)
-        session['user_id'] = user_id
-        return web.HTTPFound('/dashboard')
-    else:
-        return web.Response(text='Authentication failed', status=401)
+    return web.Response(text=f'Data: {dict(data)}')
 
 
 async def test_login_handler(request):
